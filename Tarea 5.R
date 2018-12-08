66138aca45ad1edab58397f550f0797315ea4494
aleesteban <alejandro.calderont@mail.udp.cl>
2018-12-08 00:15:44 +0000

    TERMINAMOS

--- Tarea 5.R
+++ Tarea 5.R
@@ -4,6 +4,137 @@ library(tidyquant)
 library(ggplot2)
 library(ggthemes)
 
+MSFT_price <- tq_get("MSFT",
+                     get = "stock.prices",
+                     from = "1999-11-01",
+                     to = "2018-09-05",
+                     periodicity = "monthly")
+
+MSFT_quantmod <- getSymbols("MSFT",
+                            src = "yahoo",
+                            from = "1999-11-01",
+                            to = "2018-09-05",
+                            periodicity = "monthly")
+
+AAPL_price <- tq_get("AAPL",
+                     get = "stock.prices",
+                     from = "1999-11-01",
+                     to = "2018-09-05",
+                     periodicity = "monthly")
+
+AAPL_quantmod <- getSymbols("AAPL",
+                            src = "yahoo",
+                            from = "1999-11-01",
+                            to = "2018-09-05",
+                            periodicity = "monthly")
+
+MSFT_quantmod <- as.data.frame(MSFT) %>%
+  as.tibble() %>%
+  mutate(date = index(MSFT)) %>%
+  rename("open" = "MSFT.Open", "high" = "MSFT.High",
+         "low" = "MSFT.Low" , "close"= "MSFT.Close",
+         "volume" = "MSFT.Volume", "adjusted" = "MSFT.Adjusted")
+x <- length(MSFT_price$adjusted)
+
+AAPL_quantmod <- as.data.frame(AAPL) %>%
+  as.tibble() %>%
+  mutate(date = index(AAPL)) %>%
+  rename("open" = "AAPL.Open", "high" = "AAPL.High",
+         "low" = "AAPL.Low" , "close"= "AAPL.Close",
+         "volume" = "AAPL.Volume", "adjusted" = "AAPL.Adjusted")
+y <- length(AAPL_price$adjusted)
+
+# letra (a)
+
+for (i in 1:x) {
+  MSFT_price[i+1,8] = log(MSFT_price[i+1,7] / MSFT_price[i,7])
+  if(i <= 2) {MSFT_price[i+1,9] = MSFT_price[i+1,8] + MSFT_price[i,8]}
+  else{MSFT_price[i+1,9] = MSFT_price[i,9] + MSFT_price[i+1,8]}
+  
+}
+
+for(i in 1:y){
+  AAPL_price[i+1,8] = log(AAPL_price[i+1,7]/AAPL_price[i,7])
+  if(i <= 2){AAPL_price[i+1,9] = AAPL_price[i+1,8] + AAPL_price[i,8]}
+  else{AAPL_price[i+1,9] = AAPL_price[i,9] + AAPL_price[i+1,8]}
+}
+
+MSFT_price = MSFT_price %>% rename(retornos_activos = "adjusted.1")
+MSFT_price = MSFT_price %>% rename(retornos_acum_activos = "adjusted.1.1")
+
+MSFT_retorno <- MSFT_price[3:226,]
+r1 <- length(MSFT_retorno$retornos_activos)
+sum(MSFT_retorno$retornos_activos)
+
+AAPL_price = AAPL_price %>% rename(retornos_activos = "adjusted.1")
+AAPL_price = AAPL_price %>% rename(retornos_acum_activos = "adjusted.1.1")
+
+AAPL_retorno <- AAPL_price[3:226,]
+r2 <- length(AAPL_price$retornos_activos)
+sum(AAPL_price$retornos_activos)
+
+# letra (b)
+
+#Gráficos Microsoft
+
+ggplot(MSFT_retorno) + geom_line(aes(x=date, y=retornos_activos), color = "red") + 
+  labs(title = "Retorno mensual Microsoft", subtitle = "Desde Enero 2000 hasta Agosto 2018")+ 
+  theme_tq() + scale_color_tq()
+
+ggplot(MSFT_retorno) + geom_line(aes(x=date, y=retornos_acum_activos), color = "red") + 
+  labs(title = "Retorno Acumulado mensual Microsoft", subtitle = "Desde Enero 2000 hasta Agosto 2018")+ 
+  theme_tq() + scale_color_tq()
+
+#Gráficos Apple
+
+ggplot(AAPL_retorno) + geom_line(aes(x=date, y=retornos_activos), color = "red") + 
+  labs(title = "Retorno mensual Apple", subtitle = "Desde Enero 2000 hasta Agosto 2018")+ 
+  theme_tq() + scale_color_tq()
+
+ggplot(AAPL_retorno) + geom_line(aes(x=date, y=retornos_activos), color = "red") + 
+  labs(title = "Retorno Acumulado mensual Apple", subtitle = "Desde Enero 2000 hasta Agosto 2018")+ 
+  theme_tq() + scale_color_tq()
+
+# letra (c)
+
+m1 <- sum(MSFT_retorno$retornos_activos)/r1 
+m2 <- sum((MSFT_retorno$retornos_activos - m1)^2)  
+m3 <- sum((MSFT_retorno$retornos_activos - m1)^3)  
+m4 <- sum((MSFT_retorno$retornos_activos - m1)^4) 
+
+asim1 <- (m3/m2^(3/2))^2 #calculo coeficiente de  
+asim2 <- (m4/m2^2) 
+
+#Test Jarque-Bera
+
+JB_MSFT <- r1*asim1/6+(asim2-3)^2/24
+
+#Chi-cuadrado
+
+chi_cuadrado_MSFT <- 1 - pchisq(JB_MSFT, df = 2)
+
+#H0: retornos presentan normalidad 
+#H1: retornos no presentan normalidad
+
+norm_MSFT = if(JB_MSFT > chi_cuadrado_MSFT){"Se rechaza la hipotesis nula"}else{"No se rechaza la hipotesis nula"}
+norm_MSFT
+
+a1 <- sum(AAPL_retorno$retornos_activos)/r2 
+a2 <- sum((AAPL_retorno$retornos_activos - a1)^2) 
+a3 <- sum((AAPL_retorno$retornos_activos - a1)^3)  
+a4 <- sum((AAPL_retorno$retornos_activos - a1)^4) 
+
+asima1 <- (a3/a2^(3/2))^2 
+asima2 <- (a4/a2^2) 
+
+JB_AAPL <- r2*asima1/6+(asima2-3)^2/24
+
+chi_cuadrado_AAPL <- 1 - pchisq(JB_AAPL, df = 2)
+
+norm_AAPL= if(JB_AAPL > chi_cuadrado_AAPL){"Se rechaza la hipotesis nula"}else{"No se rechaza la hipotesis nula"}
+norm_AAPL
+
+
 
 
 
